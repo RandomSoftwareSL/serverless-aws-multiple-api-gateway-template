c00e753767f1b6451fb32d5b07bf6584841c625d
Janitha Tennakoon <janitha@randomsoftware.net>
2023-10-11 10:42:03 +0530

    Add additional inputs for cli

--- helpers/cmdInputBuilder.js
+++ helpers/cmdInputBuilder.js
@@ -14,6 +14,30 @@ class CMDInputBuilder {
     return `
     Number of API Gateways (default is 1): `;
   }
+
+  getMainAPIGatewayName() {
+    return `
+    Main API Gateway name(default is main): `;
+  }
+
+  getAPIGatewayNames() {
+    return `
+    Names for additional generated API Gateways
+    (add as comma separated values): `;
+  }
+
+  getSharedAPIGatewayInput() {
+    return `
+    Does main API Gateway need to have shared serverless stacks?
+    (1) false
+    (2) true
+    (default is false): `;
+  }
+
+  getAPIGatewaySharedName() {
+    return `
+    Common path for shared gateway: `;
+  }
 }
 
 module.exports = CMDInputBuilder;
--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const main = async () => {
   const introduction = `
     Welcome to Serverless AWS Multile Api Gateway Pattern Generator.
     Please follow below steps to generate the template
+        1. Choose language (currently only supporting TypeScript only)
+        2. Number of API gateways needed to be created
+        3. Main API Gateway name (can be changed after)
+        4. Names of the API gateways in order to generate serverless stack files (if number of api gateways is more than 1)
+        5. Whether main API Gateway needs to have multiple serverless stacks
+
     `;
 
   console.log(introduction);
@@ -21,6 +27,34 @@ const main = async () => {
     inputBuilder.getNumberOfAPIGateways()
   );
   numberOfApiGateways = numberOfApiGateways === "" ? "1" : numberOfApiGateways;
+
+  let manApiGatewayName = await cmdManager(
+    inputBuilder.getMainAPIGatewayName()
+  );
+  manApiGatewayName = manApiGatewayName === "" ? "main" : manApiGatewayName;
+
+  if (parseInt(numberOfApiGateways) > 1) {
+    let apiGatewayNames = await cmdManager(inputBuilder.getAPIGatewayNames());
+    const geteWayNameLength = apiGatewayNames.split(",").length;
+
+    if (geteWayNameLength !== parseInt(numberOfApiGateways) - 1) {
+      console.log(
+        `Provided names length does not equal to given additional number of api gateways: ${
+          parseInt(numberOfApiGateways) - 1
+        }.`
+      );
+      apiGatewayNames = await cmdManager(inputBuilder.getAPIGatewayNames());
+    }
+  }
+
+  let sharedForMain = await cmdManager(inputBuilder.getSharedAPIGatewayInput());
+  sharedForMain = sharedForMain === "" ? "1" : sharedForMain;
+
+  if (sharedForMain === "2") {
+    let sharedMainPath = await cmdManager(
+      inputBuilder.getAPIGatewaySharedName()
+    );
+  }
 };
 
 main();
